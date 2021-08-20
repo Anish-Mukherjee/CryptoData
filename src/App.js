baa3e904191b67af495e4a46223aca12f36741cd
AnishMukherjee03 <AnishMukherjee03@users.noreply.github.com>
2021-08-20 21:31:12 +0530

    Init_1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-const fiftyFifty = Math.random() < 0.5;
 
-class App extends React.Component{
-  render(){
-    let task;
-    if (fiftyFifty) {
-      task = "WOO!"
-    } else {
-      task = "to bed"
-    }
-    return <h1>{task}</h1>;
-  };
-};
 
-export default App;
